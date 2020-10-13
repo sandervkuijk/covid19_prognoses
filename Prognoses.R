984439d648b36c98385600115a31fd3ceeec59a0
Bram-R <67429090+Bram-R@users.noreply.github.com>
2020-10-13 15:06:03 +0200

    IC based on https://github.com/J535D165/CoronaWatchNL and new COVID IC admissions added

--- Prognoses.R
+++ Prognoses.R
@@ -8,22 +8,16 @@ library(forecast)
 palette(c("black", "white"))
 lbls <- format(seq(as.Date("2020-3-13"), Sys.Date() + 2, by = "2 week"), "%e\n%b")
 
-###### DATA ######
-IC <- c(78, 96, 120, 150, 178, 228, 288, 366, 430, 480, 575, 657, 755, 838, 927, 
-        995, 1044, 1135, 1192, 1211, 1256, 1283, 1287, 1310, 1320, 1329, 1299, 1297, 
-        1310, 1271, 1252, 1237, 1215, 1190, 1155, 1127, 1082, 1058, 1044, 1008, 958, 
-        917, 884, 851, 836, 804, 781, 750, 724, 691, 655, 648, 636, 600, 576, 524, 494, 
-        470, 454, 447, 407, 388, 364, 350, 336, 335, 330, 298, 288, 271, 255, 243, 231, 
-        215, 192, 177, 170, 161, 152, 153, 151, 150, 136, 131, 128, 128, 128, 127, 117, 
-        115, 111, 107, 106, 107, 109, 97, 91, 81, 78, 71, 67, 68, 62, 60, 55, 51, 46, 
-        42, 42, 41, 37, 38, 35, 37, 35, 34, 33, 32, 33, 31, 29, 30, 29, 29, 28, 27, 29, 
-        29, 31, 30, 29, 30, 28, 27, 25, 26, 24, 23, 22, 25, 27, 27, 29, 33, 35, 34, 36, 
-        36, 37, 41, 39, 42, 44, 44, 50, 48, 50, 58, 54, 54, 47, 49, 53, 48, 53, 48, 46, 
-        45, 42, 43, 48, 48, 44, 42, 43, 48, 44, 46, 45, 47, 46, 49, 54, 59, 65, 66, 65, 
-        61, 61, 65, 76, 89, 98, 106, 114, 121, 136, 140, 152, 162, 170, 172, 178, 177, 
-        185, 198, 215, 239, 239) # https://stichting-nice.nl/
+###### RETRIEVE DATA ######
+dat_NICE <- fread("https://github.com/J535D165/CoronaWatchNL/blob/master/data-ic/data-nice/NICE_IC_wide_latest.csv?raw=true") # https://stichting-nice.nl/ & https://github.com/J535D165/CoronaWatchNL
+dat_RIVM <- fread("https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.csv") 
 
-dat_RIVM <- fread("https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.csv")
+# Data manipulation
+dat_NICE <- subset(dat_NICE, as.Date(dat_NICE$Datum) >= as.Date("2020-3-13")) # select same start date as RIVM data
+IC <- dat_NICE$TotaalOpnamen #Total number of IC intakes
+IC_COV_new <- dat_NICE$ToenameOpnamen #Number of newly confirmed or suspected COVID-19 IC intakes 
+IC <- IC[1:(length(IC) - 2)] #Remove last 2 rows (as these are still being updated)
+IC_COV_new <- IC_COV_new[1:(length(IC_COV_new) - 2)]  #Remove last 2 rows (as these are still being updated)
 COV <- aggregate(formula = Total_reported ~ Date_of_report, FUN = sum, data = dat_RIVM)
 COV_limb <- aggregate(formula = Total_reported ~ Date_of_report, FUN = sum, 
                       data = subset(dat_RIVM, Province == "Limburg"))
@@ -34,11 +28,15 @@ Death <- aggregate(formula = Deceased ~ Date_of_report, FUN = sum, data = dat_RI
 Death_limb <- aggregate(formula = Deceased ~ Date_of_report, FUN = sum, 
                         data = subset(dat_RIVM, Province == "Limburg"))
 
-###### DATAFRAMES ######
+###### CREATE DATAFRAMES ######
 # A = huidig aantal, I = incidentie, C = cumulatieve incidentie
 IC <- data.frame(A = IC,
                  I = IC - shift(IC, n=1, fill=0, type="lag"),
                  dag = 1:length(IC)
+)
+
+IC_COV_new <- data.frame(I = IC_COV_new,
+                         dag = 1:length(IC_COV_new)
 )
 
 COV <- data.frame(C = COV$Total_reported,
@@ -64,9 +62,9 @@ Death <- data.frame(C = Death$Deceased,
 rm(COV_limb, Hosp_limb, Death_limb) #clean workspace
 
 ###### TRENDLIJN ######
+### IC ###
 pred <- data.frame(time = seq(length(IC$dag) + 1, length.out = 7))
 
-### IC ###
 # loess
 loess <- loess(I ~ dag, IC, control = loess.control(surface = "direct"), span = 0.25)
 pred$IC_I_loess <- predict(loess, data.frame(dag = pred$time), se = TRUE)[[1]]
@@ -86,6 +84,29 @@ lines(loess$x, loess$fitted, col = "red")
 lines(pred$time, pred$IC_I_loess, col = "red", lty= 3)
 lines(loess$x, arima$fitted, col = "blue")
 lines(pred$time, pred$IC_I_arima, col = "blue", lty= 3)
+
+### IC COVID-19 NEW ###
+pred$time = seq(length(IC_COV_new$dag) + 1, length.out = 7)
+
+# loess
+loess <- loess(I ~ dag, IC_COV_new, control = loess.control(surface = "direct"), span = 0.25)
+pred$IC_COV_new_I_loess <- predict(loess, data.frame(dag = pred$time), se = TRUE)[[1]]
+pred$IC_COV_new_I_loess_se <- predict(loess, data.frame(dag = pred$time), se = TRUE)[[2]]
+
+# arima
+arima <- auto.arima(IC_COV_new$I)
+# autoplot(forecast(arima)) 
+# checkresiduals(arima)
+pred$IC_COV_new_I_arima <- summary(forecast(arima, h = length(pred$time)))[[1]]
+pred$IC_COV_new_I_arima_lo <- summary(forecast(arima, h = length(pred$time)))[[4]]
+pred$IC_COV_new_I_arima_up <- summary(forecast(arima, h = length(pred$time)))[[5]]
+
+# fit
+plot(I ~ dag, xlim = c(0, length(dag) + 7), data = IC_COV_new)
+lines(loess$x, loess$fitted, col = "red")
+lines(pred$time, pred$IC_COV_new_I_loess, col = "red", lty= 3)
+lines(loess$x, arima$fitted, col = "blue")
+lines(pred$time, pred$IC_COV_new_I_arima, col = "blue", lty= 3)
 
 ###  Positief ### 
 pred$time = seq(length(COV$dag) + 1, length.out = 7)
@@ -137,17 +158,33 @@ rm(arima, loess) #clean workspace
 pred <- pred[ , -1] #clean pred dataframe (time differs per outcome)
 
 ###### IC COVID-19 #####
+# IC TOTAL
 # Figuur - NL
 png("Figures/ICopnames_NL.png", width = 1000, height = 600, pointsize = 18)
 par(mar = c(5.1, 4.1, 4.1, 1.1))
 
 plot(IC$I ~ IC$dag, ylim = c(floor(min(IC$I)/10) * 10, ceiling(max(IC$I)/10) * 10), 
      xlim = c(0, length(IC$dag)), ylab = "", xlab = "Datum", xaxt = "n", yaxt = "n", 
-     pch = 16, cex = 0.6, main = "COVID-19 - IC opnames - verschil dag ervoor")
+     pch = 16, cex = 0.6, main = "Totaal - IC opnames - verschil dag ervoor")
 axis(side = 1, at = seq(1, length(IC$dag) + 2, 14), labels = lbls, tick = FALSE)
 tick_o <- seq(floor(min(IC$I)/10) * 10, ceiling(max(IC$I)/10) * 10, 25)
 axis(side = 2, at = tick_o)
 abline(h = tick_o, v = seq(1, by = 7, length.out = ceiling(length(IC$dag) + 9)/7), lty = 3)
+
+dev.off()
+
+# IC COVID-19 NEW
+# Figuur - NL
+png("Figures/ICopnames_COVnew_NL.png", width = 1000, height = 600, pointsize = 18)
+par(mar = c(5.1, 4.1, 4.1, 1.1))
+
+plot(IC_COV_new$I ~ IC_COV_new$dag, ylim = c(0, ceiling(max(IC_COV_new$I)/10) * 10), 
+     xlim = c(0, length(IC_COV_new$dag)), ylab = "", xlab = "Datum", xaxt = "n", yaxt = "n", 
+     pch = 16, cex = 0.6, main = "COVID-19 - nieuwe IC opnames")
+axis(side = 1, at = seq(1, length(IC_COV_new$dag) + 2, 14), labels = lbls, tick = FALSE)
+tick_o <- seq(0, ceiling(max(IC_COV_new$I)/10) * 10, 25)
+axis(side = 2, at = tick_o)
+abline(h = tick_o, v = seq(1, by = 7, length.out = ceiling(length(IC_COV_new$dag) + 9)/7), lty = 3)
 
 dev.off()
 
@@ -233,3 +270,6 @@ axis(side = 2, at = tick_o)
 abline(h = tick_o, v = seq(1, by = 7, length.out = ceiling(length(Death$dag) + 9)/7), lty = 3)
 
 dev.off()
+
+###### Save R session ######
+save.image(file="COVID19.RData") 
