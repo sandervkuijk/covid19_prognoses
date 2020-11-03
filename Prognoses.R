b96faaa47dd46a11b55de0f2e238fcf974f7e11d
Bram-R <67429090+Bram-R@users.noreply.github.com>
2020-11-03 16:04:50 +0100

    Hosp$I en IC$I obv intakes (vanuit NICE)

--- Prognoses.R
+++ Prognoses.R
@@ -17,10 +17,12 @@ lbls <- format(seq(date_start, Sys.Date() + 30, by = "2 week"), "%e %b")
 
 ###### RETRIEVE AND MANIPULATE DATA ######
 #dat_NICE <- fread("https://github.com/J535D165/CoronaWatchNL/blob/master/data-ic/data-nice/NICE_IC_wide_latest.csv?raw=true") 
-#dat_NICE_IC_int <- rjson::fromJSON(file = "https://www.stichting-nice.nl/covid-19/public/new-intake/",simplify = TRUE)
-#dat_NICE_IC_int <- dat_NICE_IC_int %>% map(as.data.table) %>% rbindlist(fill=TRUE)
-#dat_NICE_Hosp_int <- rjson::fromJSON(file = "https://www.stichting-nice.nl/covid-19/public/zkh/new-intake/",simplify = TRUE)
-#dat_NICE_Hosp_int <- dat_NICE_Hosp_int %>% map(as.data.table) %>% rbindlist(fill = TRUE)
+dat_NICE_IC_int <- rjson::fromJSON(file = "https://www.stichting-nice.nl/covid-19/public/new-intake/",simplify = TRUE)
+dat_NICE_IC_int <- dat_NICE_IC_int %>% map(as.data.table) %>% rbindlist(fill=TRUE)
+dat_NICE_IC_int <- as.data.frame(t(dat_NICE_IC_int))[, -3]
+dat_NICE_Hosp_int <- rjson::fromJSON(file = "https://www.stichting-nice.nl/covid-19/public/zkh/new-intake/",simplify = TRUE)
+dat_NICE_Hosp_int <- dat_NICE_Hosp_int %>% map(as.data.table) %>% rbindlist(fill = TRUE)
+dat_NICE_Hosp_int <- as.data.frame(t(dat_NICE_Hosp_int))[, -3]
 dat_NICE_IC_C <- rjson::fromJSON(file = "https://www.stichting-nice.nl/covid-19/public/intake-cumulative",simplify = TRUE)
 dat_NICE_IC_C <- dat_NICE_IC_C %>% map(as.data.table) %>% rbindlist(fill = TRUE)
 dat_NICE_Hosp_C <- rjson::fromJSON(file = "https://www.stichting-nice.nl/covid-19/public/zkh/intake-cumulative/",simplify = TRUE)
@@ -93,7 +95,7 @@ R0 <- data.frame(R = dat_RIVM_R[dat_RIVM_R$Type==levels(dat_RIVM_R$Type)[3], ]$W
 R0 <- subset(R0, R0$date >= date_start)
 
 Hosp <- data.frame(C = Hosp$value,
-                   I = pmax(Hosp$value - shift(Hosp$value, n=1, fill=0, type="lag"), 0),
+                   I = unlist(dat_NICE_Hosp_int[, 2]) + unlist(dat_NICE_Hosp_int[, 3]), #pmax(Hosp$value - shift(Hosp$value, n=1, fill=0, type="lag"), 0),
                    date = as.Date(Hosp$date)
 )
 Hosp$I_3d <- rollsumr(Hosp$I, k = 3, fill = NA)
@@ -101,7 +103,7 @@ Hosp <- subset(Hosp, Hosp$date >= date_start) # Select data from start date
 Hosp <- subset(Hosp, Hosp$date <= Sys.Date() - 1) # Remove todays data (as these are still being updated)
 
 IC <- data.frame(C = IC$value,
-                 I = pmax(IC$value - shift(IC$value, n=1, fill=0, type="lag"), 0),
+                 I = unlist(dat_NICE_IC_int[, 2]) + unlist(dat_NICE_IC_int[, 3]), #pmax(IC$value - shift(IC$value, n=1, fill=0, type="lag"), 0),
                  date = as.Date(IC$date)
 )
 IC$I_3d <- rollsumr(IC$I, k = 3, fill = NA)
